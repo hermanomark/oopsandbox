09acb58ec43a927f4975cbc1f5abe1756a3b4993
hermanomark <hermano.mark.mh@gmail.com>
2018-11-16 14:56:26 +0800

    intro to oop php

---
+++ 2_4.php
@@ -0,0 +1,35 @@
+<?php
+    class User {
+        public $name;
+        public $age;
+
+        //  Runs when an object is created
+        public function __construct($name, $age) {
+            echo 'Class ' . __CLASS__ . ' instantiated<br>';
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        public function sayHello() {
+            return $this->name . ' Says Hello';
+        }
+
+        // Called when no other references to a certain object
+        // Used for cleanup, closeing connections, etc.
+        public function __destruct() {
+            echo 'destructor ran...';
+        }
+    }
+
+    $user1 = new User('Brad', 36);
+
+    echo $user1->name . ' is ' . $user1->age . ' years old';
+    echo '<br>';
+    echo $user1->sayHello();
+
+    echo '<br>';
+    $user2 = new User('Mark', 23);
+    echo $user2->name . ' is ' . $user2->age . ' years old';
+    echo '<br>';
+    echo $user2->sayHello();
+?>
